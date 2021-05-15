f47741b5f370a7a1979296c24109c23cb9d5f5e2
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-05-16 00:03:54 +0300

    init

---
+++ src/models/Board.java
@@ -0,0 +1,8 @@
+package models;
+
+import interfaces.IBoard;
+
+public class Board implements IBoard
+{
+
+}
---
+++ src/models/MoveLogger.java
@@ -0,0 +1,8 @@
+package models;
+
+import interfaces.IMoveLogger;
+
+public class MoveLogger implements IMoveLogger
+{
+
+}
