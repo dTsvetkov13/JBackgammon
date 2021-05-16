22404a1420315d30e28e641028500ecd6e89a027
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-05-16 22:34:00 +0300

    Add basic methods

--- src/interfaces/IList.java
+++ src/interfaces/IList.java
@@ -1,6 +1,14 @@
 package interfaces;
 
-public interface IList
+public interface IList<T>
 {
-
+	boolean isEmpty(); 
+	boolean contains(T item); 
+	void add(T item);
+	void insertAt(T item, int index); 
+	void removeFrom(int index);
+	void remove(T item);
+	T get(int index);
+	void clear();
+	int size();
 }
