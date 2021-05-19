eff72240c8f8d18eb8e54c42b8e8da11b4bae902
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-05-19 20:02:12 +0300

    Add some methods

--- src/interfaces/IGameEventListener.java
+++ src/interfaces/IGameEventListener.java
@@ -2,5 +2,13 @@ package interfaces;
 
 public interface IGameEventListener
 {
-	
+	void throwDice();
+	void annnouceDiceThrow();
+	void announcePossiblePullDestionations();
+	void selectPull();
+	void selectPullDestionation();
+	void announcePullMoved();
+	void redo();
+	void undo();
+	void announceGameOver();
 }
