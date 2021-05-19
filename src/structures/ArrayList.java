40e3e43ffe5118d0c40f3ac3a62376a0c2581af6
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-05-19 15:13:50 +0300

    Now implements IList

--- src/structures/ArrayList.java
+++ src/structures/ArrayList.java
@@ -1,6 +1,69 @@
 package structures;
 
-public class ArrayList
-{
+import interfaces.IList;
 
+public class ArrayList<T> implements IList<T>
+{
+	@Override
+	public boolean isEmpty()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean contains(T item)
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void add(T item)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insertAt(T item, int index)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeFrom(int index)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(T item)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T get(int index)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clear()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int size()
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
