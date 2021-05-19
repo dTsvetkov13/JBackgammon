394ca4188a763d373db7106c483b84fa60ae4b70
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-05-19 22:25:25 +0300

    Implements IEventListener

--- src/models/Board.java
+++ src/models/Board.java
@@ -1,8 +1,19 @@
 package models;
 
 import interfaces.IBoard;
+import listeners.IEventListener;
 
-public class Board implements IBoard
+public class Board implements IBoard, IEventListener
 {
 
+	@Override
+	public void onEvent(Event event)
+	{
+		switch(event.type)
+		{
+			default:
+				break;
+		}
+	}
+	
 }
