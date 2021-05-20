e97e4bfd48838c2392a66b326a4f13f2ceee3450
anast <anast@DESKTOP-8RB94TT>
2021-05-20 13:01:33 +0300

    ArrayList added

--- src/structures/ArrayList.java
+++ src/structures/ArrayList.java
@@ -6,72 +6,126 @@ import interfaces.IList;
 
 public class ArrayList<T> implements IList<T>
 {
+	private int index;
+	private Object[] list;
+	
+	public ArrayList(int size) 
+	{
+		index = 0;
+		list = new Object[size];
+	}
+	
 	@Override
 	public boolean isEmpty()
 	{
-		// TODO Auto-generated method stub
-		return false;
+		return index <= 0;
 	}
 
 	@Override
 	public boolean contains(T item)
 	{
-		// TODO Auto-generated method stub
-		return false;
+		return getIndexOfTheItem(item) != -1;
 	}
 
 	@Override
 	public void add(T item)
 	{
-		// TODO Auto-generated method stub
+		list[index++] = item;
 		
+		if(index == list.length)
+		{
+			Object[] temp = list;
+			list = new Object[temp.length * 2];
+			
+			for(int i = 0; i < temp.length; i++)
+			{
+				list[i] = temp[i];
+			}
+		}
 	}
 
 	@Override
 	public void insertAt(T item, int index)
 	{
-		// TODO Auto-generated method stub
+		if(this.index + 1 == list.length)
+		{
+			Object[] temp = list;
+			list = new Object[temp.length * 2];
+			
+			for(int i = 0; i < temp.length; i++)
+			{
+				list[i] = temp[i];
+			}
+		}
 		
+		for(int i = this.index; i > index; i--)
+		{
+			list[i] = list[i - 1];
+		}
+		list[index] = item;
 	}
 
 	@Override
 	public void removeFrom(int index)
 	{
-		// TODO Auto-generated method stub
+		if(index < 0 || index >= this.index)
+		{
+			throw new IllegalArgumentException("This index doesn't exist");
+		}
 		
+		for(int i = index; i < this.index - 1; i++)
+		{
+			list[i] = list[i + 1];
+		}
 	}
 
 	@Override
 	public void remove(T item)
 	{
-		// TODO Auto-generated method stub
-		
+		int index = getIndexOfTheItem(item);
+		if(index == -1)
+		{
+			return;
+		}
+		removeFrom(index);
 	}
 
 	@Override
 	public T get(int index)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		if(index < 0 || index >= this.index)
+		{
+			throw new IllegalArgumentException("Incorrect index");
+		}
+		return (T) list[index];
 	}
 
 	@Override
 	public void clear()
 	{
-		// TODO Auto-generated method stub
-		
+		index = 0;
 	}
 
 	@Override
 	public int size()
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		return index;
 	}
 
 	@Override
 	public Iterator<T> iterator() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	private int getIndexOfTheItem(T item)
+	{
+		for(int i = 0; i<this.index; i++)
+		{
+			if(list[i].equals(item))
+				return i;
+		}
+			
+		return -1;
 	}
 }
