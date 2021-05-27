b360d1997542b462e36232ba628790da0ddd3d33
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-05-27 16:52:30 +0300

    Add board and window fields and constructor declaration

--- src/core/Game.java
+++ src/core/Game.java
@@ -1,6 +1,24 @@
 package core;
 
-public class Game
-{
+import javax.swing.JFrame;
 
+import listeners.IEventListener;
+import models.Board;
+import models.Event;
+
+public class Game implements IEventListener
+{
+	private Board board;
+	private JFrame window;
+	
+	public Game()
+	{
+		
+	}
+	
+	@Override
+	public void onEvent(Event event)
+	{
+		
+	}
 }
