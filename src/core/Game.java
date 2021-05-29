baa9850f65c0f5e4feaa1c0a6a33cb5716c40694
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-05-29 17:30:36 +0300

    Create drawing window with board

--- src/core/Game.java
+++ src/core/Game.java
@@ -1,5 +1,7 @@
 package core;
 
+import java.awt.Rectangle;
+
 import javax.swing.JFrame;
 
 import listeners.IEventListener;
@@ -13,7 +15,14 @@ public class Game implements IEventListener
 	
 	public Game()
 	{
-		
+		board = new Board();
+		window = new JFrame();
+		window.setBounds(new Rectangle(100, 100, 1000, 1000));
+		window.setLayout(null);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+		board.setBounds(50, 50, 800, 800);
+		window.add(board);
 	}
 	
 	@Override
