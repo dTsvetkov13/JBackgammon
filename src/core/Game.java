6ae473b519ac6078ee39e12a33bd23bdc01dea8c
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-06-09 21:41:58 +0300

    Add menu and Game screen changing

--- src/core/Game.java
+++ src/core/Game.java
@@ -1,5 +1,6 @@
 package core;
 
+import java.awt.CardLayout;
 import java.awt.Rectangle;
 
 import javax.swing.JFrame;
@@ -7,27 +8,41 @@ import javax.swing.JFrame;
 import listeners.IEventListener;
 import models.Board;
 import models.Event;
+import views.Menu;
 
 public class Game implements IEventListener
 {
 	private Board board;
 	private JFrame window;
+	private Menu menu;
+	private CardLayout layout;
 	
 	public Game()
 	{
-		board = new Board();
 		window = new JFrame();
 		window.setBounds(new Rectangle(100, 100, 1000, 1000));
-		window.setLayout(null);
+		layout = new CardLayout();
+		window.setLayout(layout);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
-		board.setBounds(50, 50, 800, 800);
-		window.add(board);
+		menu = new Menu();
+		window.add(menu);
 	}
 	
 	@Override
 	public void onEvent(Event event)
 	{
-		
+		switch(event.type)
+		{
+		case StartGame:
+		{
+			board = new Board();
+			window.add(board);
+			this.layout.previous(this.window.getContentPane());
+			break;
+		}
+		default:
+			break;
+		}
 	}
 }
