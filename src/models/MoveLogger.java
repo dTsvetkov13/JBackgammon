c396e8d440fdccc493ab4f011136aeb8d740b6b8
SashoN1913 <Sashko@DESKTOP-9CLBQ45>
2021-06-09 22:02:08 +0300

    Add basic structure

--- src/models/MoveLogger.java
+++ src/models/MoveLogger.java
@@ -1,8 +1,42 @@
 package models;
 
-import interfaces.IMoveLogger;
+import java.util.LinkedList;
 
-public class MoveLogger implements IMoveLogger
+import interfaces.ICommand;
+import interfaces.IMoveLogger;
+import interfaces.IStack;
+import structures.Stack;
+
+public class MoveLogger implements IMoveLogger 
 {
+	private Stack<Log> undo;
+	private Stack<Log> redo;
+	
+	@Override
+	public void onEvent(Event event) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void logMove(Position from, Position to) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void undo() 
+	{
+		undo.pop();
+		//redo.push(data);
+	}
+	
+	@Override
+	public void redo() 
+	{
+		
+	}
 
 }
