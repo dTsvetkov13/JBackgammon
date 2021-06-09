1f8fbcfdc1625c8be219b2f70e6720f8b82e44ca
SashoN1913 <Sashko@DESKTOP-9CLBQ45>
2021-06-09 22:30:48 +0300

    Add currentDice parameter to logMove()

--- src/interfaces/IMoveLogger.java
+++ src/interfaces/IMoveLogger.java
@@ -2,10 +2,11 @@ package interfaces;
 
 import listeners.IEventListener;
 import models.Position;
+import structures.Pair;
 
 public interface IMoveLogger extends IEventListener
 {
-	void logMove(Position from, Position to);
+	void logMove(Position from, Position to, Pair<Integer, Integer> currentDice);
 	void undo();
 	void redo();
 }
