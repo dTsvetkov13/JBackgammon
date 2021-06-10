331a636585d7025785db0e25efa2f09f433bfa2d
SashoN1913 <Sashko@DESKTOP-9CLBQ45>
2021-06-10 22:27:56 +0300

    Add pull setSize() and setPull()

--- src/tests/PullViewTest.java
+++ src/tests/PullViewTest.java
@@ -1,20 +1,22 @@
 package tests;
 
 import javax.swing.JFrame;
-
+import common.Team;
 import views.PullView;
 
-public class PullViewTest {
+public class PullViewTest 
+{
 
 	public static void main(String[] args) 
 	{
 		PullView pull = new PullView();
+		pull.setSize(32, 32);
+		pull.setPull(Team.Black);
 		JFrame window = new JFrame();
-		window.add(pull);
 		window.setSize(800, 800);
 		window.setLayout(null);
 		window.setVisible(true);
-
+		window.add(pull);
 	}
 
 }
