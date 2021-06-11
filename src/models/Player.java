180652b9edf9c4b6b542de34838ade1f75f8c95e
anast <anast@DESKTOP-8RB94TT>
2021-06-11 08:32:46 +0300

    Add constructors

--- src/models/Player.java
+++ src/models/Player.java
@@ -5,4 +5,15 @@ import common.Team;
 public class Player 
 {
 	private Team team;
+
+	public Player()
+	{
+		this(Team.Black);
+	}
+	
+	public Player(Team team) 
+	{
+		super();
+		this.team = team;
+	}	
 }
